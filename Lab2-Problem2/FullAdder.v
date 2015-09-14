13708130b233d2524f312dd563bbdd9ccdb27950
Adam White <adam.nathaniel.white@gmail.com>
2015-09-14 08:10:35 -0400

    Remove unnecessary directory

--- Lab2-Problem2/FullAdder.v
+++
@@ -1,9 +0,0 @@
- module fulladder (s,co,a,b,ci);
-    output s,co;
-    input a,b,ci;
-    xor1 u1(s,a,b,ci);
-    and1 u2(n1,a,b);
-    and1 u3(n2,b,ci);
-    and1 u4(n3,a,ci);
-    or1 u5(co,n1,n2,n3);
-endmodule
