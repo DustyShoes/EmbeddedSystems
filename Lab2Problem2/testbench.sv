331501bdae193871b4b8040952a60d0fc25cdf15
Adam White <adam.nathaniel.white@gmail.com>
2015-09-20 14:26:08 -0400

    Add more complete testing for Lab 2, Problem 2. Test just some useful inputs only, in addition to all possible input combinations.

--- Lab2Problem2/testbench.sv
+++ Lab2Problem2/testbench.sv
@@ -24,6 +24,168 @@ Lab2Problem2 test (
 	.reset(reset)
 );
 
+initial begin
+	// initialize inputs
+	reset = 1'b0;
+	Select[0] = 0;
+	Select[1] = 0;
+	Select[2] = 0;
+	C = 1'b0;
+	B[0] = 0;
+	B[1] = 0;
+	B[2] = 0;
+	B[3] = 0;
+	A[0] = 0;
+	A[1] = 0;
+	A[2] = 0;
+	A[3] = 0;
+	clock = 0;
+	reset = 0;
+	
+	//000 not A
+	#50
+	
+	A = 4'b1010;
+	#50
+	
+	//001 A+B+C
+	
+	Select[0] = 0;
+	Select[1] = 0;
+	Select[2] = 1;
+	A = 4'b0000;
+	B = 4'b0000;
+	C = 1'b0;
+	#200
+	
+	A = 4'b1010;
+	B = 4'b1010;
+	C = 0;
+	#200
+	
+	A = 4'b0101;
+	B = 4'b0101;
+	C = 0;
+	#200
+	
+	A = 4'b0000;
+	B = 4'b0000;
+	C = 1;
+	#200
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	C = 1;
+	#200
+	
+	//010 A and B
+	Select[0] = 0;
+	Select[1] = 0;
+	Select[2] = 1;
+	A = 4'b0000;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b0000;
+	B = 4'b1111;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	#100
+	
+	//011 A or B
+	Select[0] = 0;
+	Select[1] = 1;
+	Select[2] = 1;
+	A = 4'b0000;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b0000;
+	B = 4'b1111;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	#100
+	
+	//100 A xor B
+	Select[0] = 1;
+	Select[1] = 0;
+	Select[2] = 0;
+	A = 4'b0000;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b0000;
+	B = 4'b1111;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b0000;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	#100
+	
+	//101 A, C rotate
+	Select[0] = 1;
+	Select[1] = 0;
+	Select[2] = 1;
+	A = 4'b0000;
+	C = 0;
+	#100
+	
+	A = 4'b0000;
+	C = 1;
+	#100
+	
+	A = 4'b1111;
+	C = 0;
+	#100
+	
+	A = 4'b1000;
+	C = 4'b0001;
+	#100
+	
+	//110 0
+	Select[0] = 1;
+	Select[1] = 1;
+	Select[2] = 0;
+	A = 4'b0000;
+	B = 4'b0000;
+	C = 0;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	C = 1;
+	#100
+	
+	//111 1
+	Select[0] = 1;
+	Select[1] = 1;
+	Select[2] = 1;
+	A = 4'b0000;
+	B = 4'b0000;
+	C = 0;
+	#100
+	
+	A = 4'b1111;
+	B = 4'b1111;
+	C = 1;
+end
+
 //clock
 always #200 clock = ~clock;
 
@@ -81,25 +243,5 @@ always @ (posedge clock) begin
 	end
 	reset = ~reset;
 end
-
-initial begin
-	// initialize inputs
-	reset = 1'b0;
-	Select[0] = 0;
-	Select[1] = 0;
-	Select[2] = 0;
-	C = 1'b0;
-	B[0] = 0;
-	B[1] = 0;
-	B[2] = 0;
-	B[3] = 0;
-	A[0] = 0;
-	A[1] = 0;
-	A[2] = 0;
-	A[3] = 0;
-	clock = 0;
-	reset = 0;
-end
-
 
 endmodule
