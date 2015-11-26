f9257dc7fc656043fa3d84307475c8699abfc50b
Adam White <adam.nathaniel.white@gmail.com>
2015-11-25 23:07:18 -0500

    Add the Quartus .qpf files

--- SortProject/inputnSort.v
+++ SortProject/inputnSort.v
@@ -17,18 +17,18 @@ module inputnSort(
 	input 		    [1:0]		KEY, // the push button key in the FPGA board
 
 	//////////// LED //////////
-	output		    [4:0]		LED,// the LED to display the outputs
+	output		    [3:0]		LED,// the LED to display the outputs
 
 	//////////// SW //////////
 	input 		    [3:0]		SW, // THe switch to give the ALU inputs to the board
 
-	input reg even_clk
+	input even_clk
 );	//port declarations
 
 	
 	//internal variables
 
-	parameter n	=	4; //bit lenght is 4 bits
+	parameter k	=	4; //bit lenght is 4 bits
 	parameter N	=	4; //no.of numbers
 
 	//Reg and Wire Declaration
@@ -36,9 +36,10 @@ module inputnSort(
 
 	wire 	clk;
 	wire 	reset;	//for reset 
-	reg 	[n-1:0] 	inp_reg;	//the inputs are temporarily stored in this
-	reg	[n-1:0] 	reg_out;
+	reg 	[k-1:0] 	inp_reg;	//the inputs are temporarily stored in this
+	reg	[k-1:0] 	reg_out;
 	reg inputs_done; //to indicate that all the N inputs are given by the user
+	//reg even_clk;
 
 	//assigning internal signal to the QSF mapped signals///////////
 	//--------------------------------------
@@ -46,15 +47,15 @@ module inputnSort(
 	assign reset 		= 	KEY[0];
 	assign clk              =  	FPGA_CLK1_50;
 	assign LED[3:0]         =  	reg_out;
-	reg [n-1 : 0] myrows [0 : N-1];	
+	reg [k-1 : 0] myrows [0 : N-1];	
 	integer i;
 	//internal counter
 	reg	[5:0]	counter; // no.of bits depend on N and so change this value accordingly
-	reg		key;
+	reg		mykey;
 	reg		key_delay;
 	wire		pulses;
 	wire		pulse_input;	
-	assign pulses		=	key ^ key_delay;
+	assign pulses		=	mykey ^ key_delay;
 	assign pulse_input 	=	pulses & key_delay;
 	
 	
@@ -77,8 +78,8 @@ always @(posedge clk or negedge reset) begin
 	end // reset if ends here
 	
 	else begin
-		key		<=	KEY[1];
-		key_delay	<=	key;
+		mykey		<=	KEY[1];
+		key_delay	<=	mykey;
 		if(pulse_input) begin
 					counter  <=	counter + 1'b1;		
 					if(counter == N+1) begin // not N, we used an extra register inp_reg and counter=0 is reset
